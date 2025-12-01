8730c0baa650c5829aef7cdf0242e0183713420e
Julian Walter <julianwalter1593@gmail.com>
2025-12-01 23:45:18 +0100

    :sparkles: added logging to endpoint

--- src/main/java/com/tschayjay/priceanalysis/controller/MarketController.java
+++ src/main/java/com/tschayjay/priceanalysis/controller/MarketController.java
@@ -1,48 +1,30 @@
 package com.tschayjay.priceanalysis.controller;
 
 import com.tschayjay.priceanalysis.dto.MarketDto;
-import com.tschayjay.priceanalysis.model.Market;
 import com.tschayjay.priceanalysis.service.MarketService;
-import com.tschayjay.priceanalysis.service.impl.MarketServiceImpl;
-import com.vaadin.flow.i18n.I18NProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Locale;
 
 /**
  * @author julianwalter
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-class MarketController implements I18NProvider {
+class MarketController {
 
     private final MarketService marketService;
-    
-    @GetMapping("/market")
-    ResponseEntity<String> getMarkets() {
-        List<Market> markets = marketService.getMarkets();
-        return ResponseEntity.ok("success");
-    }
 
     @PostMapping("/market")
     ResponseEntity<String> createMarkets(@RequestBody List<MarketDto> dto) {
         marketService.processApiCall(dto);
+        log.info("Processed {} market information", dto.size());
         return ResponseEntity.ok("success");
-    }
-
-    @Override
-    public List<Locale> getProvidedLocales() {
-        return List.of();
-    }
-
-    @Override
-    public String getTranslation(String s, Locale locale, Object... objects) {
-        return "";
     }
 }
